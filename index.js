c87bada15dfb2c7f6685802694b4ab5d25480b90
hiba <hibzrashed@gmail.com>
2022-05-02 21:42:16 -0500

    commit tree d4b1fc83fd65a1c24898c5c924de03ac628223fc

---
+++ index.js
@@ -0,0 +1,29 @@
+function add() {
+    return "something";
+}
+function add(a, b) {
+    return a + b;
+} function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return ++n;
+}
+function decrement(n) {
+    return --n;
+}
+function makeInt(n) {
+    return parseInt(n, 10); 
+    return NaN;
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+    return NaN;
+}
+   
